803f4cb33b9208be8c98a0902b3182686431fbb8
Mohammad Hizzani <moh.hizzani@gmail.com>
2019-12-26 20:13:03 +0200

    comment out the old crossentropy function

--- src/lossFuns.jl
+++ src/lossFuns.jl
@@ -6,26 +6,26 @@
     output:
         the average of the cross entropy loss function
 """
-function crossentropy(Ŷ, Y)
-    m = length(Y)
-    newŶ = copy(Ŷ)
-
-    """
-        return previous float if x == 1 and nextfloat if x == 0
-    """
-    prevnextfloat(x) = x==0 ? nextfloat(x) : x==1 ? prevfloat(x) : x
-
-    newŶ = prevnextfloat.(newŶ)
-
-    J = -sum(Y .* log.(newŶ) .+ (1 .- Y) .* log.(1 .- newŶ))/m
-    return J
-end #crossentropy
+# function crossentropy(Ŷ, Y)
+#     m = length(Y)
+#     newŶ = copy(Ŷ)
+#
+#     """
+#         return previous float if x == 1 and nextfloat if x == 0
+#     """
+#     prevnextfloat(x) = x==0 ? nextfloat(x) : x==1 ? prevfloat(x) : x
+#
+#     newŶ = prevnextfloat.(newŶ)
+#
+#     J = -sum(Y .* log.(newŶ) .+ (1 .- Y) .* log.(1 .- newŶ))/m
+#     return J
+# end #crossentropy
 
 function crossentropy(a, y)
     aNew = prevnextfloat(a)
     J = -(y * log(aNew) + (1-y) * log(1-aNew))
     return J
-end
+end #crossentropy
 
 export crossentropy
 
