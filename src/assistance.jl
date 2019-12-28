e57b720ed7cf9a27a28170fba517074df59f718c
Mohammad Hizzani <moh.hizzani@gmail.com>
2019-12-28 21:53:41 +0200

    create the oneHot function

--- src/assistance.jl
+++ src/assistance.jl
@@ -1,6 +1,20 @@
 
-function oneHot(Y)
-
+"""
+    convert array of integer classes into one Hot coding
+"""
+function oneHot(Y; classes = [], numC = 0)
+    if numC > 0 || length(classes) > 0
+        Cs = sort(classes)
+        c = numC
+    else
+        Cs = sort(unique(Y))
+        c = length(Cs)
+    end
+    hotY = BitArray{2}(undef, c, 0)
+    for y in Y
+        hotY = hcat(hotY, Integer.(Cs .== y))
+    end
+    return hotY
 end
 
 export oneHot
