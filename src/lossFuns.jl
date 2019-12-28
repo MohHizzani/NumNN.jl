516adf2e4955cca5c49485b944ba6b7e72e85c8d
Mohammad Hizzani <moh.hizzani@gmail.com>
2019-12-28 16:00:33 +0200

    make the crossentropy and its deriv operate over vectors

--- src/lossFuns.jl
+++ src/lossFuns.jl
@@ -1,29 +1,21 @@
+
 """
+    return the average cross entropy loss over vector of labels and predictions
+
     input:
-        Ŷ := (1,m) matrix of predicted labels
-        Y := (1,m) matrix of true      labels
+        a := (c,1) matrix of predicted values, where c is the number of classes
+        y := (c,1) matrix of predicted values, where c is the number of classes
+
+        Note: in case the number of classes is one (1) it is okay to have
+              a scaler values for a and y
 
     output:
-        the average of the cross entropy loss function
+        J := scaler value of the cross entropy loss
 """
-# function crossentropy(Ŷ, Y)
-#     m = length(Y)
-#     newŶ = copy(Ŷ)
-#
-#     """
-#         return previous float if x == 1 and nextfloat if x == 0
-#     """
-#     prevnextfloat(x) = x==0 ? nextfloat(x) : x==1 ? prevfloat(x) : x
-#
-#     newŶ = prevnextfloat.(newŶ)
-#
-#     J = -sum(Y .* log.(newŶ) .+ (1 .- Y) .* log.(1 .- newŶ))/m
-#     return J
-# end #crossentropy
-
 function crossentropy(a, y)
-    aNew = prevnextfloat(a)
-    J = -(y * log(aNew) + (1-y) * log(1-aNew))
+
+    aNew = prevnextfloat.(a)
+    J = sum(.-(y .* log.(aNew) .+ (1 .- y) .* log.(1 .- aNew)))
     return J
 end #crossentropy
 
@@ -33,14 +25,8 @@ export crossentropy
     compute the drivative of cross-entropy loss function
 """
 function dcrossentropy(a, y)
-
-    """
-        return previous float if x == 1 and nextfloat if x == 0
-    """
-    prevnextfloat(x) = x==0 ? nextfloat(x) : x==1 ? prevfloat(x) : x
-
-    aNew = prevnextfloat(a)
-    dJ = -(y/aNew - (1-y)/(1-aNew))
+    aNew = prevnextfloat.(a)
+    dJ = .-(y ./ aNew .- (1 .- y) ./ (1 .- aNew))
     return dJ
 end #dcrossentropy
 
@@ -50,3 +36,5 @@ export dcrossentropy
     return previous float if x == 1 and nextfloat if x == 0
 """
 prevnextfloat(x) = x==0 ? nextfloat(x) : x==1 ? prevfloat(x) : x
+
+export prevnextfloat
