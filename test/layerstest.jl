d0cf93cb37f1b6a5e7ac433e4634d469daa55022
Mohammad Hizzani <moh.hizzani@gmail.com>
2020-02-17 16:59:39 +0200

    some tests lines

--- test/layerstest.jl
+++ test/layerstest.jl
@@ -1,13 +1,47 @@
-using NumNN
+include("../src/includes.jl")
 
-x = rand(20,5);
+# using .NumNN
 
-Y = rand(0:4, 5);
+x = rand(64*64,256);
+
+Y = rand(0:4, 256);
 Y = oneHot(Y, classes = [0,1,2,3,4])
-
-X1 = NumNN.FCLayer(20, :relu)(x)
+#
+X1 = FCLayer(50, :relu)(x)
+X2 = FCLayer(20, :relu)(X1)
 X = FCLayer(20, :tanh)(X1)
-X = AddLayer(X, X1)
+X = AddLayer()([X, X2])
 X = FCLayer(5, :softmax)(X)
 
+
+# X1 = FCLayer(20, :relu)(x)
+# X = FCLayer(20, :tanh)(X1)
+# X = AddLayer()([X,X1])
+# X = FCLayer(5, :softmax)(X)
+# deepInitWB!(x, X)
+
+# resetCount!(X, :forwCount)
+
+# deepInitVS!(X, :adam)
+
 mo = Model(x, Y, X, 0.01; optimizer = :adam)
+
+Ŷ_pred = predict(mo, x, Y)
+
+chainBackProp!(x,Y,mo)
+
+Ŷ = chainForProp(x, X)
+
+x1 = randn(64*64, 5);
+
+ŷ1 = chainForProp(x1, X);
+
+X = FCLayer(120, :relu)(x)
+
+X = FCLayer(5, :softmax)(X)
+
+mo1 = Model(x, Y, X, 0.01; optimizer = :adam)
+
+ŷ_oneLayer = chainForProp(x, X);
+
+ŷ_oneLayer_pred = predict(mo1, x, Y)
