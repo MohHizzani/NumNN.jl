88c18a05b61b3671568629dc9de2f2824dca8f41
Mohammad Hizzani <moh.hizzani@gmail.com>
2020-02-24 18:06:18 +0200

    some test lines for cnn models

---
+++ test/cnntest.jl
@@ -0,0 +1,13 @@
+include("../src/includes.jl")
+
+x = rand(1:255, 10,10,3,10) #10x10x3 images 10 of them
+Y = rand(0:4, 10)
+Y = oneHot(Y; classes = [0,1,2,3,4])
+
+####create some Layers
+
+X = Conv2D(10, (3,3))(x)
+X = Conv2D(20, (3,3))(X)
+X = Conv2D(20, (1,1))(X)
+#### test initialization
+deepInitWB!(x, X; dtype=Float64)
