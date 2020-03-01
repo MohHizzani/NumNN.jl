e08f5f633188f4f9e1c61b5e8e47059f500a4aa3
Mohammad Hizzani <moh.hizzani@gmail.com>
2020-03-01 15:47:16 +0200

    layer-centric chainForProp!

--- src/backForProp.jl
+++ src/backForProp.jl
@@ -3,13 +3,14 @@ using ProgressMeter
 using Random
 using LinearAlgebra
 
+include("layerForProp.jl")
 
 """
     perform the chained forward propagation using recursive calls
 
     input:
     X := input of the forward propagation
-    oLayer := output layer
+    cLayer := output layer
     cnt := an internal counter used to cache the layers was performed
            not to redo it again
 
@@ -20,56 +21,38 @@ using LinearAlgebra
         to be used later in back propagation and add one to the layer
         forwCount value when pass through it
 """
-function chainForProp(X, oLayer::Layer, cnt::Integer=-1)
+function chainForProp!(X, cLayer::Layer, cnt::Integer=-1)
     if cnt<0
-        cnt = oLayer.forwCount+1
+        cnt = cLayer.forwCount+1
     end
 
-    if oLayer.prevLayer==nothing
-        actFun = oLayer.actFun
-        W, B = oLayer.W, oLayer.B
-        if oLayer.forwCount < cnt
-            oLayer.forwCount += 1
-            Z = W*X .+ B
-            oLayer.Z = Z
-            A = eval(:($actFun($Z)))
-            oLayer.A = A
-        else #if oLayer.forwCount < cnt
-            A = oLayer.A
-        end #if oLayer.forwCount < cnt
-        return A
-    elseif isa(oLayer, AddLayer) #if typeof(oLayer)==AddLayer
-        if oLayer.forwCount < cnt
-            oLayer.forwCount += 1
-            A = chainForProp(X, oLayer.prevLayer[1], oLayer.forwCount)
-            for prevLayer in oLayer.prevLayer[2:end]
-                A .+=  chainForProp(X, prevLayer, oLayer.forwCount)
+    if cLayer.prevLayer isa Input
+        if cLayer.forwCount < cnt
+            cLayer.forwCount += 1
+            layerForProp!(cLayer)
+        end #if cLayer.forwCount < cnt
+        return nothing
+    elseif isa(cLayer, AddLayer) #if typeof(cLayer)==AddLayer
+        if cLayer.forwCount < cnt
+            cLayer.forwCount += 1
+            for prevLayer in cLayer.prevLayer
+                chainForProp(X, prevLayer, cnt)
             end
-            oLayer.A = A
-        else #if oLayer.forwCount < cnt
-            A = oLayer.A
-        end #if oLayer.forwCount < cnt
+        end #if cLayer.forwCount < cnt
 
-        return A
-    else #if oLayer.prevLayer==nothing
-        actFun = oLayer.actFun
-        W, B = oLayer.W, oLayer.B
-        prevLayer = oLayer.prevLayer
-        if oLayer.forwCount < cnt
-            oLayer.forwCount += 1
-            Z = W*chainForProp(X, prevLayer, oLayer.forwCount) .+ B
-            oLayer.Z = Z
-            A = eval(:($actFun($Z)))
-            oLayer.A = A
-        else #if oLayer.forwCount < cnt
-            A = oLayer.A
-        end #if oLayer.forwCount < cnt
-        return A
-    end #if oLayer.prevLayer!=nothing
+        return nothing
+    else #if cLayer.prevLayer==nothing
+        if cLayer.forwCount < cnt
+            cLayer.forwCount += 1
+            chainForProp!(X, cLayer.prevLayer)
+            layerForProp!(cLayer)
+        end #if cLayer.forwCount < cnt
+        return nothing
+    end #if cLayer.prevLayer!=nothing
 
-end #function chainForProp
+end #function chainForProp!
 
-export chainForProp
+export chainForProp!
 
 
 
@@ -86,34 +69,19 @@ export chainForProp
         "Yhat_bools" := the predicted labels
         "accuracy" := the accuracy of the predicted labels
 """
-function predict(model::Model, X, Y)
-    Ŷ = chainForProp(X, model.outLayer)
+function predict(
+    model::Model,
+    X::Array,
+    Y=nothing,
+    )
+    chainForProp!(X, model.outLayer)
+    Ŷ = model.outLayer.A
     T = eltype(Ŷ)
-    c, m = size(Y)
     outLayer = model.outLayer
+    actFun = outLayer.actFun
     # if isbool(Y)
-    acc = 0
-    if isequal(outLayer.actFun, :softmax)
-        # Ŷ_bool = BitArray(undef, c, 0)
-        maximums = maximum(Ŷ, dims=1)
-        Ŷ_bool = Ŷ .== maximums
-        T = eltype(Y)
-        # Ŷ_bool = T.(Ŷ_bool)
-        for i=1:m
-            acc += (Ŷ_bool[:,i] == Y[:,i]) ? 1 : 0
-        end
-        acc /= m
-        println("Accuracy is = $acc")
-    end
+    return predict(eval(:($actFun)), Ŷ, labels=Y)
 
-    if isequal(outLayer.actFun, :σ)
-        Ŷ_bool = Ŷ .> T(0.5)
-        acc = sum(Ŷ_bool .== Y)/(c*m)
-        println("Accuracy is = $acc")
-    end
-    return Dict("Yhat"=>Ŷ,
-                "Yhat_bool"=>Ŷ_bool,
-                "accuracy"=>acc)
 end #predict
 
 export predict
@@ -126,159 +94,12 @@ function isbool(y::Array{T}) where {T}
 end
 
 
-#TODO remove all tMiniBatch from update functions
+
+### back propagation
+
+include("layerBackProp.jl")
 
 
-
-"""
-    do the back propagation for the output layer
-"""
-function outBackProp!(model::Model, Y, cnt::Integer; tMiniBatch::Integer=1)
-    outLayer = model.outLayer
-    prevLayer = outLayer.prevLayer
-    lossFun = model.lossFun
-    m = size(Y)[2]
-
-    A, Z = outLayer.A, outLayer.Z
-
-    regulization, λ = model.regulization, model.λ
-
-    if outLayer.backCount < cnt
-        outLayer.backCount += 1
-
-        if outLayer.keepProb < 1.0 #to save memory and time
-            D = rand(outLayer.numNodes,1) .< outLayer.keepProb
-            outLayer.A = outLayer.A .* D
-            outLayer.A = outLayer.A ./ outLayer.keepProb
-        end
-
-
-        dlossFun = Symbol("d",lossFun)
-        actFun = outLayer.actFun
-
-        outLayer.dZ = eval(:($dlossFun($A, $Y)))
-
-
-        outLayer.dW = outLayer.dZ*prevLayer.A' ./m
-
-        if regulization > 0
-            if regulization==1
-                outLayer.dW .+= (λ/2m)
-            else
-                outLayer.dW .+= (λ/m) .* outLayer.W
-            end
-        end
-
-
-
-
-        outLayer.dB = 1/m .* sum(outLayer.dZ, dims=2)
-
-        # updateParams!(model,
-        #               outLayer,
-        #               tMiniBatch)
-
-    end #if outLayer.backCount < cnt
-
-
-
-    return nothing
-end #function outBackProp!
-
-export outBackProp!
-
-
-function backProp!(X::Array,
-                   model::Model,
-                   cLayer::Layer,
-                   cnt::Integer;
-                   tMiniBatch::Integer)
-
-    outLayer = model.outLayer
-    prevLayer = cLayer.prevLayer
-    lossFun = model.lossFun
-    keepProb = 1.0
-    try
-        keepProb = cLayer.keepProb
-    catch
-
-    end
-    m = size(X)[2]
-    A, Z = [], []
-    try
-        A, Z = cLayer.A, cLayer.Z
-    catch
-
-    end
-
-    regulization, λ = model.regulization, model.λ
-
-    ## in case nothing has finished yet of the next layer(s)
-    if cLayer.backCount >= cnt
-        return nothing
-    end
-
-    if all(i->(i.backCount==cLayer.nextLayers[1].backCount), cLayer.nextLayers)
-        if cLayer isa AddLayer
-            for nextLayer in cLayer.nextLayers
-                try
-                    cLayer.dZ .+= nextLayer.W'nextLayer.dZ
-                catch e
-                    cLayer.dZ = nextLayer.W'nextLayer.dZ
-                end #tyr/catch
-            end #for
-            cLayer.backCount += 1
-            return nothing
-        end #if cLayer isa AddLayer
-        dA = []
-        for nextLayer in cLayer.nextLayers
-            try
-                dA .+= nextLayer.W'nextLayer.dZ
-            catch e
-                try
-                    dA = nextLayer.W'nextLayer.dZ
-                catch e1
-                    dA = nextLayer.dZ
-                end #try/catch
-            end #try/catch
-        end #for
-        if keepProb < 1.0 #to save time of multiplication in case keepProb was one
-           D = rand(cLayer.numNodes,1) .< keepProb
-           dA = dA .* D
-           dA = dA ./ keepProb
-        end
-    dActFun = Symbol("d",cLayer.actFun)
-
-    cLayer.dZ = dA .* eval(:($dActFun($Z)))
-
-    try ##in case it is the input layer
-        cLayer.dW = cLayer.dZ*cLayer.prevLayer.A' ./m
-    catch e
-        cLayer.dW = cLayer.dZ*X' ./m
-    end #try/cathc
-
-    if regulization > 0
-        if regulization==1
-            cLayer.dW .+= (λ/2m)
-        else
-            cLayer.dW .+= (λ/m) .* cLayer.W
-        end
-    end
-
-    cLayer.dB = 1/m .* sum(cLayer.dZ, dims=2)
-
-    cLayer.backCount += 1
-
-    # updateParams!(model,
-    #               cLayer,
-    #               tMiniBatch)
-
-    end #if all(i->(i.backCount==cLayer.nextLayers[1].backCount), cLayer.nextLayers)
-
-    return nothing
-end
-
-export backProp!
 
 """
 
