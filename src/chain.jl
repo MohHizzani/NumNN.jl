ce00641cf37ca838f72808ec241f5880794ac5e9
Mohammad Hizzani <moh.hizzani@gmail.com>
2020-03-03 16:32:42 +0200

    chaining for each pool layer

--- src/chain.jl
+++ src/chain.jl
@@ -60,6 +60,54 @@ function (l::Conv3D)(li_1::Layer)
     return l
 end
 
+function (l::MaxPool1D)(li_1::Layer)
+    l.prevLayer = li_1
+    if ! in(l,li_1.nextLayers)
+        push!(li_1.nextLayers, l)
+    end
+    return l
+end
+
+function (l::MaxPool2D)(li_1::Layer)
+    l.prevLayer = li_1
+    if ! in(l,li_1.nextLayers)
+        push!(li_1.nextLayers, l)
+    end
+    return l
+end
+
+function (l::MaxPool3D)(li_1::Layer)
+    l.prevLayer = li_1
+    if ! in(l,li_1.nextLayers)
+        push!(li_1.nextLayers, l)
+    end
+    return l
+end
+
+function (l::AveragePool1D)(li_1::Layer)
+    l.prevLayer = li_1
+    if ! in(l,li_1.nextLayers)
+        push!(li_1.nextLayers, l)
+    end
+    return l
+end
+
+function (l::AveragePool2D)(li_1::Layer)
+    l.prevLayer = li_1
+    if ! in(l,li_1.nextLayers)
+        push!(li_1.nextLayers, l)
+    end
+    return l
+end
+
+function (l::AveragePool3D)(li_1::Layer)
+    l.prevLayer = li_1
+    if ! in(l,li_1.nextLayers)
+        push!(li_1.nextLayers, l)
+    end
+    return l
+end
+
 function (l::Activation)(li_1::Layer)
     l.prevLayer = li_1
     try
