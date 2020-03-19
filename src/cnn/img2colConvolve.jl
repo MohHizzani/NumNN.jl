bbd4db64da7b1b46be28b69e4ee1b82b8dccf5ad
Mohammad Hizzani <moh.hizzani@gmail.com>
2020-03-19 16:04:01 +0200

    img2colConvolve functions

--- src/cnn/img2colConvolve.jl
+++ src/cnn/img2colConvolve.jl
@@ -41,3 +41,65 @@ end #function img2colConvolve(cLayer::Conv3D
 
 
 export img2colConvolve!
+
+### dimg2colConvolve!
+
+export dimg2colConvolve!
+
+function dimg2colConvolve!(
+    cLayer::Conv1D,
+    Ai::AbstractArray{T,3},
+    dA::AbstractArray{T,3},
+    dZ::AbstractArray{T,3},
+) where {T}
+
+    cLayer.dA = col2img3D(cLayer.K' * img2col(dZ), cLayer.outputS)
+    cLayer.dK = dZ * Ai'
+    cLayer.dB = sum(permutedims(dZ, [1, 3, 2]), dims = 1:2)
+
+    return nothing
+end # function dimg2colConvolve!(
+    #     cLayer::Conv1D,
+    #     Ai::AbstractArray{T,3},
+    #     dA::AbstractArray{T,3},
+    #     dZ::AbstractArray{T,3},
+    # ) where {T}
+
+
+function dimg2colConvolve!(
+    cLayer::Conv2D,
+    Ai::AbstractArray{T,4},
+    dA::AbstractArray{T,4},
+    dZ::AbstractArray{T,4},
+) where {T}
+
+    cLayer.dA = col2img3D(cLayer.K' * img2col(dZ), cLayer.outputS)
+    cLayer.dK = dZ * Ai'
+    cLayer.dB = sum(permutedims(dZ, [1, 2, 4, 3]), dims = 1:3)
+
+    return nothing
+end # function dimg2colConvolve!(
+    #     cLayer::Conv2D,
+    #     Ai::AbstractArray{T,4},
+    #     dA::AbstractArray{T,4},
+    #     dZ::AbstractArray{T,4},
+    # ) where {T}
+
+function dimg2colConvolve!(
+    cLayer::Conv3D,
+    Ai::AbstractArray{T,5},
+    dA::AbstractArray{T,5},
+    dZ::AbstractArray{T,5},
+) where {T}
+
+    cLayer.dA = col2img3D(cLayer.K' * img2col(dZ), cLayer.outputS)
+    cLayer.dK = dZ * Ai'
+    cLayer.dB = sum(permutedims(dZ, [1, 2, 3, 5, 4]), dims = 1:4)
+
+    return nothing
+end # function dimg2colConvolve!(
+    #     cLayer::Conv3D,
+    #     Ai::AbstractArray{T,5},
+    #     dA::AbstractArray{T,5},
+    #     dZ::AbstractArray{T,5},
+    # ) where {T}
