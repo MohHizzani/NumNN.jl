dc273389922375ca2b0677c00a7c4480037a773b
Mohammad Hizzani <moh.hizzani@gmail.com>
2020-03-21 19:39:24 +0200

    dfastPooling!

--- src/cnn/fastPooling.jl
+++ src/cnn/fastPooling.jl
@@ -122,3 +122,103 @@ function fastPooling!(
 end #function fastPooling!(cLayer::ThreeD,
 
 export fastPooling!
+
+### dfastPooling
+
+
+function dfastPooling!(
+    cLayer::OneD,
+    Ai::AbstractArray{T,3},
+    dAi::AbstractArray{T,3},
+    Ao::AbstractArray{T,3},
+    dAo::AbstractArray{T,3},
+) where {OneD<:Union{MaxPool1D,AveragePool1D},T}
+
+    n_Hi, c, m = size(Ai)
+    s_H = cLayer.s
+    f_H = cLayer.f
+    n_H = (n_Hi - f_H) ÷ s_H + 1
+
+    mask = reshape(Ai, f_H, n_H, c, m) .== reshape(Ao, 1, n_H, c, m)
+    dAi .= reshape(reshape(Ao, 1, n_H, c, m) .* mask, n_Hi, c, m)
+
+    mask = nothing
+    return nothing
+end #function dfastPooling!(cLayer::OneD
+
+
+function dfastPooling!(
+    cLayer::TwoD,
+    Ai::AbstractArray{T,4},
+    dAi::AbstractArray{T,4},
+    Ao::AbstractArray{T,4},
+    dAo::AbstractArray{T,4},
+) where {TwoD<:Union{MaxPool2D,AveragePool2D},T}
+
+    n_Hi, n_Wi, c, m = size(Ai)
+    s_H, s_W = cLayer.s
+    f_H, f_W = cLayer.f
+    n_H = (n_Hi - f_H) ÷ s_H + 1
+    n_W = (n_Wi - f_W) ÷ s_W + 1
+
+    mask =
+        permutedims(
+            reshape(Ai, f_H, n_H, f_W, n_W, c, m),
+            [1, 3, 2, 4, 5, 6],
+        ) .== reshape(Ao, 1, 1, n_H, n_W, c, m)
+    dAi .= reshape(
+        permutedims(
+            reshape(dAo, 1, 1, n_H, n_W, c, m) .* mask,
+            [1, 3, 2, 4, 5, 6],
+        ),
+        n_Hi,
+        n_Wi,
+        c,
+        m,
+    )
+
+    mask = nothing
+    return nothing
+end #function dfastPooling!(cLayer::TwoD,
+
+
+function dfastPooling!(
+    cLayer::ThreeD,
+    Ai::AbstractArray{T,5},
+    dAi::AbstractArray{T,5},
+    Ao::AbstractArray{T,5},
+    dAo::AbstractArray{T,5},
+) where {ThreeD<:Union{MaxPool3D,AveragePool3D},T}
+
+    n_Hi, n_Wi, n_Di, c, m = size(Ai)
+    s_H, s_W, s_D = cLayer.s
+    f_H, f_W, f_D = cLayer.f
+
+    n_H = (n_Hi - f_H) ÷ s_H + 1
+    n_W = (n_Wi - f_W) ÷ s_W + 1
+    n_D = (n_Di - f_D) ÷ s_D + 1
+
+
+    mask =
+        permutedims(
+            reshape(Ai, f_H, n_H, f_W, n_W, f_D, n_D, c, m),
+            [1, 3, 5, 2, 4, 6, 7, 8],
+        ) .== reshape(Ao, 1, 1, 1, n_H, n_W, n_D, c, m)
+
+    dAi .= reshape(
+        permutedims(
+            reshape(dAo, 1, 1, 1, n_H, n_W, n_D, c, m) .* mask,
+            [1, 4, 2, 5, 3, 6, 7, 8],
+        ),
+        n_Hi,
+        n_Wi,
+        n_Di,
+        c,
+        m,
+    )
+
+
+    return nothing
+end #function dfastPooling!(cLayer::ThreeD,
+
+export dfastPooling!
