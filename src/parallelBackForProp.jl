ee93e1dc6eeaef7040f399d1a771d11193df7ce0
Mohammad Hizzani <moh.hizzani@gmail.com>
2020-03-24 16:15:27 +0200

    create new cache based method for FCLayers

---
+++ src/parallelBackForProp.jl
@@ -0,0 +1,61 @@
+include("parallelLayerForProp.jl")
+
+
+###
+
+"""
+    perform the chained forward propagation using recursive calls
+
+    input:
+    X := input of the input layer
+    cLayer := Input Layer
+    cnt := an internal counter used to cache the layers was performed
+           not to redo it again
+
+    returns:
+    Cache := the output each layer either A, Z or together As Dict of layer to dict of Symbols and Arrays
+
+    for internal use, it set again the values of Z and A in each layer
+        to be used later in back propagation and add one to the layer
+        forwCount value when pass through it
+"""
+function chainForProp(X::AbstractArray{T,N}, cLayer::Layer, cnt::Integer=-1; FCache=Dict{Layer,Dict{Symbol, AbstractArray}}(), kwargs...) where {T,N}
+    if cnt<0
+        cnt = cLayer.forwCount+1
+    end
+
+    if length(cLayer.nextLayers) == 0
+        if cLayer.forwCount < cnt
+            FCache[cLayer] = layerForProp(cLayer; FCache = FCache, kwargs...)
+        end #if cLayer.forwCount < cnt
+        return FCache
+    elseif isa(cLayer, AddLayer) #if typeof(cLayer)==AddLayer
+        if all(
+            i -> (i.forwCount == cLayer.prevLayer[1].forwCount),
+            cLayer.prevLayer,
+        )
+            FCache[cLayer] = layerForProp(cLayer; FCache = FCache, kwargs...)
+            for nextLayer in cLayer.nextLayers
+                return chainForProp(X, nextLayer, cnt; FCache = FCache, kwargs...)
+            end
+        end #if all
+
+        return FCache
+    else #if cLayer.prevLayer==nothing
+        if cLayer.forwCount < cnt
+            if cLayer isa Input
+                FCache[cLayer] = layerForProp(cLayer, X; FCache = FCache, kwargs...)
+            else
+                FCache[cLayer] = layerForProp(cLayer; FCache = FCache, kwargs...)
+            end
+            for nextLayer in cLayer.nextLayers
+                return chainForProp(X, nextLayer, cnt; FCache = FCache, kwargs...)
+            end
+
+        end #if cLayer.forwCount < cnt
+        return FCache
+    end #if cLayer.prevLayer!=nothing
+
+end #function chainForProp!
+
+export chainForProp
---
+++ src/parallelLayerForProp.jl
@@ -0,0 +1,124 @@
+using Statistics
+
+###Input Layer
+function layerForProp(
+    cLayer::Input,
+    X::AbstractArray = Array{Any,1}(undef,0);
+    FCache::Dict{Layer,Dict{Symbol, AbstractArray}},
+    kwargs...,
+)
+    if length(X) != 0
+        cLayer.A = X
+        cLayer.inputS = cLayer.outputS = size(X)
+    end
+    cLayer.forwCount += 1
+    # Base.GC.gc()
+    return Dict(:A=>X)
+end
+
+
+###FCLayer forprop
+
+function layerForProp(
+    cLayer::FCLayer,
+    Ai::AbstractArray = Array{Any,1}(undef,0);
+    FCache::Dict{Layer,Dict{Symbol, AbstractArray}},
+    kwargs...,
+)
+    prevLayer = cLayer.prevLayer
+    if length(Ai) == 0
+        Ai = FCache[prevLayer][:A]
+    end
+    cLayer.inputS = cLayer.prevLayer.outputS
+    Z = cLayer.W * Ai .+ cLayer.B
+    actFun = cLayer.actFun
+    # Z = cLayer.Z
+    cLayer.outputS = size(Z)
+    A = eval(:($actFun($Z)))
+    cLayer.forwCount += 1
+    # Base.GC.gc()
+    return Dict(:Z=>Z, :A=>A)
+end #function layerForProp(cLayer::FCLayer)
+
+
+###AddLayer forprop
+
+function layerForProp(
+    cLayer::AddLayer;
+    FCache::Dict{Layer,Dict{Symbol, AbstractArray}},
+    kwargs...,
+    )
+    A = similar(cLayer.prevLayer[1].A) .= 0
+    # if all(
+    #     i -> (i.forwCount == cLayer.prevLayer[1].forwCount),
+    #     cLayer.prevLayer,
+    # )
+        for prevLayer in cLayer.prevLayer
+            A .+= FCache[prevLayer][:A]
+        end
+    # end #if all
+    cLayer.forwCount += 1
+    # Base.GC.gc()
+    return Dict(:A=>A)
+end #function layerForProp(cLayer::AddLayer)
+
+###Activation forprop
+
+
+function layerForProp(
+    cLayer::Activation,
+    Ai::AbstractArray = Array{Any,1}(undef,0);
+    FCache::Dict{Layer,Dict{Symbol, AbstractArray}},
+    kwargs...,
+)
+    prevLayer = cLayer.prevLayer
+    if length(Ai) == 0
+        Ai = FCache[prevLayer][:A]
+    end
+    actFun = cLayer.actFun
+    A = eval(:($actFun($Ai)))
+    cLayer.inputS = cLayer.outputS = size(Ai)
+    cLayer.forwCount += 1
+    # Ai = nothing
+    # Base.GC.gc()
+    return Dict(:A=>A)
+end #function layerForProp(cLayer::Activation)
+
+
+###BatchNorm
+
+function layerForProp(
+    cLayer::BatchNorm,
+    Ai::AbstractArray = Array{Any,1}(undef,0);
+    FCache::Dict{Layer,Dict{Symbol, AbstractArray}},
+    kwargs...,
+)
+
+    prevLayer = cLayer.prevLayer
+    if length(Ai) == 0
+        Ai = FCache[prevLayer][:A]
+    end
+
+    initWB!(cLayer)
+
+    μ = mean(Ai, dims = 1:cLayer.dim)
+    Ai_μ = Ai .- cLayer.μ
+    N = prod(size(Ai)[1:cLayer.dim])
+    Ai_μ_s = Ai_μ .^ 2
+    var = sum(cLayer.Ai_μ_s, dims = 1:cLayer.dim) ./ N
+    Z = Ai_μ ./ sqrt.(cLayer.var .+ cLayer.ϵ)
+    A = cLayer.W .* cLayer.Z .+ cLayer.B
+    cLayer.forwCount += 1
+    Ai_μ = nothing
+    cLayer.inputS = cLayer.outputS = size(Ai)
+    # Base.GC.gc()
+    return Dict(
+        :μ => μ,
+        :Ai_μ_s => Ai_μ_s,
+        :var => var,
+        :Z => Z,
+        :A => A,
+    )
+end #function layerForProp(cLayer::BatchNorm)
+
+export layerForProp
