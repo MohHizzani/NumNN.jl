4279c9474ad5554fc4bebc3d63f1ca7f97d375de
Mohammad Hizzani <moh.hizzani@gmail.com>
2020-03-30 17:30:44 +0300

    remove commented out code

--- src/cnn/parallelImg2colConvolve.jl
+++ src/cnn/parallelImg2colConvolve.jl
@@ -12,35 +12,7 @@ function img2colConvolve(cLayer::CL, Ai::AbstractArray{T,N}) where {T,N, CL <: C
     Ao = eval(:($actFun($Z)))
 
     return Dict(:Z => Z, :A => Ao)
-end #function img2colConvolve(cLayer::Conv1D
-
-#
-# function img2colConvolve(cLayer::Conv2D, Ai::AbstractArray{T,4}) where {T}
-#
-#     Aip = padding(cLayer, Ai)
-#
-#     Z = col2img2D(cLayer.K * img2col(Aip), cLayer.outputS) .+
-#             cLayer.B[:, :, 1, :]
-#
-#     actFun = cLayer.actFun
-#     Ao = eval(:($actFun($Z)))
-#
-#     return Dict(:Z => Z, :A => Ao)
-# end #function img2colConvolve(cLayer::Conv2D
-#
-# function img2colConvolve(cLayer::Conv3D, Ai::AbstractArray{T,5}) where {T}
-#
-#     Aip = padding(cLayer, Ai)
-#
-#     Z = col2img3D(cLayer.K * img2col(Aip), cLayer.outputS) .+
-#             cLayer.B[:, :, :, 1, :]
-#
-#     actFun = cLayer.actFun
-#     Ao = eval(:($actFun($Z)))
-#
-#     return Dict(:Z => Z, :A => Ao)
-# end #function img2colConvolve(cLayer::Conv3D
-
+end #function img2colConvolve(cLayer::CL
 
 export img2colConvolve
 
