d5bbce6aefc05477506b7ad28380eb0a7b292b18
Mohammad Hizzani <moh.hizzani@gmail.com>
2020-04-05 18:51:39 +0300

    move include optimizers to TypeDef

--- src/includes.jl
+++ src/includes.jl
@@ -2,7 +2,6 @@
 include("TypeDef.jl")
 include("lossFuns.jl")
 include("actFuns.jl")
-include("optimizers.jl")
 include("initializations.jl")
 include("cnn/includes.jl")
 # include("backForProp.jl")
